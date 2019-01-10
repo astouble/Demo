1cfcee10470a40d205cfdadf8213da39c3f7a62e
libaoxin <2381500811@qq.com>
2019-01-10 21:45:44 +0800

    第二次提交

--- src/demo/test/simulation/list/DemoList.java
+++ src/demo/test/simulation/list/DemoList.java
@@ -3,5 +3,6 @@ package demo.test.simulation.list;
 public class DemoList {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
