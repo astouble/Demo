5494b33ed08d053cc0c836181ecf22b0e31c6762
libaoxin <2381500811@qq.com>
2019-01-17 22:32:31 +0800

    修改

--- src/main/java/test/simulation/list/DemoList.java
+++ src/main/java/test/simulation/list/DemoList.java
@@ -1,9 +1,14 @@
-package demo.test.simulation.list;
+package test.simulation.list;
 
 public class DemoList {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        methOne();
+    }
+    
+    public static void methOne() {
+    	methOne();
     }
 }
